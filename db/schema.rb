00c8b1885640c24d8b7a11983db3d620a70304e5
robcelin <rob.celin@kellogg.northwestern.edu>
2017-05-11 12:56:27 -0500

    Until 1 hour mark

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509154225) do
+ActiveRecord::Schema.define(version: 20170511175059) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "dob"
+    t.text     "bio"
+    t.string   "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "directors", force: :cascade do |t|
     t.string   "name"
